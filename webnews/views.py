3dedb76620032581b76166c5f9bab8a75cf1338f
Alessa <yvoangel@gmail.com>
2023-03-29 23:32:26 +0200

    Create index views\urls\templates. Works as expected. Server OK

--- webnews/views.py
+++ webnews/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from webnews.models import Newspaper, Redactor, Topic
 
-# Create your views here.
+
+def index(request):
+    num_newspaper = Newspaper.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_topic = Topic.objects.count()
+    # Показывает количество посещений страницы
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+    context = {
+        "num_newspaper": num_newspaper,
+        "num_redactors": num_redactors,
+        "num_topic": num_topic,
+        "num_visits": num_visits,
+    }
+    return render(request, "webnews/index.html", context=context)
+
+# def hello_world(request):
+#     return HttpResponse("Hello World!")
